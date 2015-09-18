533857a05c0a14bd967255c21a0ad5398ab6629d
Kristof <kristof@aut.bme.hu>
2015-09-18 11:55:05 +0200

    VisitorObserverPelda += observer functionality

--- VisitorObserverPelda/VisitorObserverPelda/main.cpp
+++ VisitorObserverPelda/VisitorObserverPelda/main.cpp
@@ -14,18 +14,55 @@ public:
     virtual void Visit(ElementString& element) = 0;
 };
 
+class ObserverBase
+{
+public:
+    virtual void ValueChanged(int oldValue, int newValue) = 0;
+    virtual void ValueChanged(string oldValue, string newValue) = 0;
+};
+
+class Observer : public ObserverBase
+{
+public:
+    virtual void ValueChanged(int oldValue, int newValue) override
+    {
+        cout << "Int value changed from " << oldValue << " to " << newValue << endl;
+    }
+
+    virtual void ValueChanged(string oldValue, string newValue) override
+    {
+        cout << "String value changed from " << oldValue << " to " << newValue << endl;
+    }
+};
+
+
 class ElementBase
 {
+protected:
+    vector<ObserverBase*> observers;
 public:
     virtual void Show() = 0;
     virtual void Accept(VisitorBase& visitor) = 0;
+    void RegisterObserver(ObserverBase *observer)
+    {
+        observers.push_back(observer);
+    }
 };
 
 class ElementInt : public ElementBase
 {
-public:
-    // A kod tomorsege erdekeben ez most public.
     int value;
+public:
+    int GetValue() { return value; }
+    void SetValue(int newValue)
+    {
+        int oldValue = value;
+        value = newValue;
+        for(auto observer : observers)
+        {
+            observer->ValueChanged(oldValue, newValue);
+        }
+    }
 
     ElementInt(const int value = 0)
         : value(value)
@@ -45,9 +82,18 @@ public:
 
 class ElementString : public ElementBase
 {
-public:
-    // A kod tomorsege erdekeben ez most public.
     string value;
+public:
+    string GetValue() { return value; }
+    void SetValue(string newValue)
+    {
+        string oldValue = value;
+        value = newValue;
+        for(auto observer : observers)
+        {
+            observer->ValueChanged(oldValue, newValue);
+        }
+    }
 
     ElementString(const string value)
         : value(value)
@@ -69,9 +115,9 @@ class ElementContainer
 {
     vector<std::unique_ptr<ElementBase>> elements;
 public:
-    void AddAndTakeOwnership(ElementBase *element)
+    void AddAndTakeOwnership(std::unique_ptr<ElementBase>& element)
     {
-        elements.push_back(unique_ptr<ElementBase>(element));
+        elements.push_back(std::move(element));
     }
 
     void Show()
@@ -82,7 +128,6 @@ public:
             element->Show();
         }
     }
-
 
     void Accept(VisitorBase& visitor)
     {
@@ -98,26 +143,40 @@ class Visitor : public VisitorBase
 public:
     virtual void Visit(ElementInt& element) override
     {
-        element.value += 100;
+        element.SetValue(element.GetValue() + 100);
     }
 
     virtual void Visit(ElementString& element) override
     {
-        element.value = element.value + "+100";
+        element.SetValue(element.GetValue() + "+100");
     }
 };
 
 int main()
 {
     ElementContainer container;
-    container.AddAndTakeOwnership(new ElementInt(1));
-    container.AddAndTakeOwnership(new ElementString("1"));
-    container.AddAndTakeOwnership(new ElementInt(2));
-    container.AddAndTakeOwnership(new ElementString("2"));
+    Observer observer;
+
+    unique_ptr<ElementBase> newElement = std::make_unique<ElementInt>(1);
+    newElement->RegisterObserver(&observer);
+    container.AddAndTakeOwnership(newElement);
+
+    newElement = std::make_unique<ElementString>("1");
+    newElement->RegisterObserver(&observer);
+    container.AddAndTakeOwnership(newElement);
+
+    newElement = std::make_unique<ElementInt>(2);
+    newElement->RegisterObserver(&observer);
+    container.AddAndTakeOwnership(newElement);
+
+    newElement = std::make_unique<ElementString>("2");
+    newElement->RegisterObserver(&observer);
+    container.AddAndTakeOwnership(newElement);
 
     cout << "--- Before visit:" << endl;
     container.Show();
 
+    cout << "--- During visit:" << endl;
     Visitor visitor;
     container.Accept(visitor);
 
