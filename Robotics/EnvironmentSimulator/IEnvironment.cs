e470a533cbfbf34f1a1133b452d972d6e4826897
Kristof <kristof@aut.bme.hu>
2020-02-10 14:12:06 +0100

    RelativePositionedScan

--- Robotics/EnvironmentSimulator/IEnvironment.cs
+++ Robotics/EnvironmentSimulator/IEnvironment.cs
@@ -8,6 +8,7 @@ namespace EnvironmentSimulator
         double Speed { get; set; }
         double Direction { get; set; }
         double Turn { get; set; }
+        Point Position { get; set; }
 
         event OnSpeedChangedDelegate OnSpeedChanged;
         event OnDirectionChangedDelegate OnDirectionChanged;
@@ -15,13 +16,24 @@ namespace EnvironmentSimulator
 
         void Tick();
 
+        Point GetLocationOfRelativePoint(double relativeDirection, double distance);
+
         /// <summary>
         /// Returns the values of the map along the scanline (x1,y1)-(x2,y2)
         /// </summary>
         IEnumerable<int> Scan(int x1, int y1, int x2, int y2);
+
+        IEnumerable<int> ScanRelative(double relativeDirection1, double distance1,
+            double relativeDirection2, double distance2);
     }
 
     public delegate void OnSpeedChangedDelegate(double newValue);
     public delegate void OnDirectionChangedDelegate(double newValue);
     public delegate void OnTickDelegate();
+
+    public struct Point
+    {
+        public int X;
+        public int Y;
+    }
 }
--- Robotics/EnvironmentSimulatorTests/Scanner.cs
+++ Robotics/EnvironmentSimulatorTests/Scanner.cs
@@ -1,4 +1,5 @@
 ﻿using EnvironmentSimulator;
+using System;
 using System.Linq;
 using Xunit;
 
@@ -6,6 +7,7 @@ namespace EnvironmentSimulatorTests
 {
     public class Scanner
     {
+
         [Fact]
         public void Instantiate()
         {
@@ -18,7 +20,40 @@ namespace EnvironmentSimulatorTests
             Assert.Equal(1, scan[2]);
             Assert.Equal(1, scan[5]);
             Assert.Equal(0, scan[6]);
+        }
 
+        [Fact]
+        public void RelativePositionCalculation()
+        {
+            IEnvironment env = new DefaultSimulator(null);
+            env.Position = new Point() { X = 0, Y = 0 };
+            env.Direction = 0.0;
+            Point pLeft = env.GetLocationOfRelativePoint(-30.0, 10);
+            Assert.Equal(-0.5 * 10.0, pLeft.X);
+            Assert.Equal(Math.Round(-Math.Sqrt(3)/ 2.0 * 10.0), pLeft.Y);
+            Point pRight = env.GetLocationOfRelativePoint(30.0, 10);
+            Assert.Equal(0.5 * 10.0, pRight.X);
+            Assert.Equal(Math.Round(-Math.Sqrt(3) / 2.0 * 10.0), pRight.Y);
+        }
+
+
+        [Fact]
+        public void RelativePositionedScan()
+        {
+            Map map = new Map(100, 100);
+            map.SetRect(9, 0, 11, 20, 1); // Vertical strip
+            IEnvironment env = new DefaultSimulator(map);
+            env.Position = new Point() { X = 10, Y = 15 };
+            env.Direction = 0.0;
+
+            var scan = env.ScanRelative(-30.0, 10, +30.0, 10).ToArray();
+            Assert.Equal(11, scan.Length);
+            for (int i = 0; i <= 3; i++)
+                Assert.Equal(0, scan[i]);
+            for (int i = 4; i <= 6; i++)
+                Assert.Equal(1, scan[i]);
+            for (int i = 7; i <= 10; i++)
+                Assert.Equal(0, scan[i]);
         }
     }
 }
