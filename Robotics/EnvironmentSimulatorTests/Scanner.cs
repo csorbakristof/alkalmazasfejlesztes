ef8a36b2cab00c78085d435ab533dd7992bdb56e
Kristof <kristof@aut.bme.hu>
2020-02-10 14:17:02 +0100

    Unit test refactor

--- Robotics/EnvironmentSimulatorTests/Scanner.cs
+++ Robotics/EnvironmentSimulatorTests/Scanner.cs
@@ -8,12 +8,21 @@ namespace EnvironmentSimulatorTests
     public class Scanner
     {
 
+        private Map map;
+        private IEnvironment env;
+
+        public Scanner()
+        {
+            map = new Map(100, 100);
+            map.SetRect(10, 10, 20, 20, 1);
+            env = new DefaultSimulator(map);
+            env.Direction = 0.0;
+        }
+
         [Fact]
         public void Instantiate()
         {
-            Map map = new Map(100, 100);
             map.SetRect(10, 10, 20, 20, 1);
-            IEnvironment env = new DefaultSimulator(map);
             var scan = env.Scan(15, 9, 21, 15).ToArray();
             Assert.Equal(7, scan.Length);
             Assert.Equal(0, scan[0]);
@@ -25,9 +34,7 @@ namespace EnvironmentSimulatorTests
         [Fact]
         public void RelativePositionCalculation()
         {
-            IEnvironment env = new DefaultSimulator(null);
             env.Position = new Point() { X = 0, Y = 0 };
-            env.Direction = 0.0;
             Point pLeft = env.GetLocationOfRelativePoint(-30.0, 10);
             Assert.Equal(-0.5 * 10.0, pLeft.X);
             Assert.Equal(Math.Round(-Math.Sqrt(3)/ 2.0 * 10.0), pLeft.Y);
@@ -36,15 +43,11 @@ namespace EnvironmentSimulatorTests
             Assert.Equal(Math.Round(-Math.Sqrt(3) / 2.0 * 10.0), pRight.Y);
         }
 
-
         [Fact]
         public void RelativePositionedScan()
         {
-            Map map = new Map(100, 100);
             map.SetRect(9, 0, 11, 20, 1); // Vertical strip
-            IEnvironment env = new DefaultSimulator(map);
             env.Position = new Point() { X = 10, Y = 15 };
-            env.Direction = 0.0;
 
             var scan = env.ScanRelative(-30.0, 10, +30.0, 10).ToArray();
             Assert.Equal(11, scan.Length);
