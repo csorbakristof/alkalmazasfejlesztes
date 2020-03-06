63214d8fa3c11e5b63bc12ebaefbe03725cf1915
Kristof <kristof@aut.bme.hu>
2020-03-06 15:06:59 +0100

    View models added

---
+++ Robotics/Viewer/ViewModel/MapViewModel.cs
@@ -0,0 +1,42 @@
+﻿using Environment;
+using System.Collections.Generic;
+using Windows.UI;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace Viewer.ViewModel
+{
+    public class MapViewModel
+    {
+        public WriteableBitmap Image;
+
+        public MapViewModel(Map map)
+        {
+            Image = ImageFromMap(map);
+        }
+
+        private WriteableBitmap ImageFromMap(Map map)
+        {
+            var img = new WriteableBitmap(map.SizeX, map.SizeY);
+            var colorDict = new Dictionary<byte, Color>
+            {
+                { 0, Colors.White },
+                { 1, Colors.Blue },
+                { 255, Colors.Gray }
+            };
+
+            using (img.GetBitmapContext())
+            {
+                for (int x = 0; x < map.SizeX; x++)
+                {
+                    for (int y = 0; y < map.SizeY; y++)
+                    {
+                        byte mapValue = (byte)(map[x, y]);
+                        img.SetPixel(x, y, colorDict[mapValue]);
+                    }
+                }
+            }
+
+            return img;
+        }
+    }
+}
---
+++ Robotics/Viewer/ViewModel/RobotViewModel.cs
@@ -0,0 +1,58 @@
+﻿using Robot;
+using System;
+using System.ComponentModel;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace Viewer.ViewModel
+{
+    public class RobotViewModel : INotifyPropertyChanged
+    {
+        // Shape (position and orientation and sensor ranges, sensor values)
+        private readonly LineAndWallDetectorRobot robot;
+
+        public double X => robot.Location.X;
+        public double Y => robot.Location.Y;
+        public Single Orientation => Convert.ToSingle(robot.Orientation);
+
+        public BitmapImage Image;
+
+        public RobotViewModel(LineAndWallDetectorRobot robot)
+        {
+            this.robot = robot;
+            this.Image = new BitmapImage
+            {
+                UriSource = new Uri(@"ms-appx:///Assets/Robot.png")
+            };
+            InitAndStartPropertyChangedTimer();
+        }
+
+        #region INCP triggered by timer
+        private DispatcherTimer timer;
+        private void InitAndStartPropertyChangedTimer()
+        {
+            timer = new DispatcherTimer();
+            timer.Tick += Timer_Tick;
+            timer.Interval = new TimeSpan(0, 0, 1);
+            timer.Start();
+        }
+
+        // Now we use a timer to trigger the updates, not a full INCP chain starting from the model.
+        private void Timer_Tick(object sender, object e)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(this.X)));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(this.Y)));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(this.Orientation)));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        internal void TriggerSimulationTick()
+        {
+            // Note: called by the UI timer to trigger simulation ticks.
+            robot.Environment.Tick();
+        }
+        #endregion
+    }
+}
