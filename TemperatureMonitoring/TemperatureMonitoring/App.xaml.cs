d35c9079095f6b54701acbc5d348a6e29e6938e4
kristof <csorba.kristof@vik.bme.hu>
2025-03-07 10:38:04 +0100

    TemperatureMonitoring initial commit

---
+++ TemperatureMonitoring/TemperatureMonitoring/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TemperatureMonitoring
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
